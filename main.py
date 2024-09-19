102d5b9ffc2d610aa685313ee2c10ab3ccb29e61
Maksim Ushakov <dmeaaxd@gmail.com>
2024-09-19 15:16:29 +0300

    delete gateway url

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ def push_gauge_metric(metric, name, documentation):
 
     g = Gauge(name=name, documentation=documentation, registry=registry)
     g.set(metric)
-    pushadd_to_gateway('147.45.108.82:9091', job='custom_metrics', registry=registry, handler=my_auth_handler)
+    pushadd_to_gateway(os.getenv('GATEWAY'), job='custom_metrics', registry=registry, handler=my_auth_handler)
+
 
 
 if __name__ == "__main__":
